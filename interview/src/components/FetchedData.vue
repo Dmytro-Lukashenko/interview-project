c81ea7d055c9828f88b2e967c857f6e15bb7fe6b
Dmitry Lukashenko <d.a.lukashenko@gmail.com>
2022-05-31 12:42:41 +0300

    add db.json

---
+++ interview/src/components/FetchedData.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            questions: []
+        }
+    },
+    async mounted(){
+        const responce = await fetch('@db.json')
+        const fetchedQuestions = await responce.json()
+        console.log(fetchedQuestions)
+    }
+}
+</script>
+
+<style>
+
+</style>
